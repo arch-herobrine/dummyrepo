cf64f2afe3eeb60589e25c790083d574f6a906c2
arch-herobrine <yuuma9314@outlook.jp>
2022-11-10 11:48:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import urllib.request
+import time
+for i in range(5):
+    url = 'https://surlfs.f5.si'
 
-url = 'https://surlfs.f5.si'
-
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    body = res.read()
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
 
 
-url2 = 'https://surlfs-mk2.f5.si'
-req2 = urllib.request.Request(url2)
-with urllib.request.urlopen(req2) as res2:
-    body2 = res2.read()
+    url2 = 'https://surlfs-mk2.f5.si'
+    req2 = urllib.request.Request(url2)
+    with urllib.request.urlopen(req2) as res2:
+        body2 = res2.read()
+    time.sleep(60)
