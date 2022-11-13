e410e415dc7a915ec0aab887470595dac51f615b
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2022-11-13 15:02:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ for i in range(60):
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         body = res.read()
+        
+    url2 = "https://authbot-on-TH.arch-herobrine.repl.co"
+    with urllib.request.urlopen(req2) as res2:
+        body2 = res2.read()
     time.sleep(60)
 
    
