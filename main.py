c833896f8839c5b8d09b550a52f6ed726e9aa33c
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2022-11-14 11:11:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ for i in range(60):
     req2 = urllib.request.Request(url2)
     with urllib.request.urlopen(req2) as res2:
         body2 = res2.read()
+        
+    url3 = 'https://surlfs.f5.si'
+    req3 = urllib.request.Request(url3)
+    with urllib.request.urlopen(req3) as res3:
+        body3 = res3.read()
     time.sleep(60)
 
    
