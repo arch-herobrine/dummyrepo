8472ddaa1144df20961d1e3b2eee5f11000449cd
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2022-11-16 12:14:07 +0900

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ for i in range(60):
     with urllib.request.urlopen(req) as res:
         body = res.read()
         
-    url2 = "https://authbot-on-TH.arch-herobrine.repl.co"
+    url2 = "https://archmusicbot-1.arch-herobrine.repl.co"
     req2 = urllib.request.Request(url2)
     with urllib.request.urlopen(req2) as res2:
         body2 = res2.read()
@@ -16,6 +16,19 @@ for i in range(60):
     req3 = urllib.request.Request(url3)
     with urllib.request.urlopen(req3) as res3:
         body3 = res3.read()
+
+    url4 = 'https://archmusicbot-2.arch-herobrine.repl.co'
+    req4 = urllib.request.Request(url4)
+    with urllib.request.urlopen(req4) as res4:
+        body4 = res4.read()
+    url4 = 'https://archmusicbot-3.arch-herobrine.repl.co'
+    req4 = urllib.request.Request(url4)
+    with urllib.request.urlopen(req4) as res4:
+        body4 = res4.read()
+    url4 = 'https://archmusicbot-4.arch-herobrine.repl.co'
+    req4 = urllib.request.Request(url4)
+    with urllib.request.urlopen(req4) as res4:
+        body4 = res4.read()
     time.sleep(60)
 
    
